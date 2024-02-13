935a0922de8c8bf1de59b0534dfda8f5e3833e9e
Viacheslav Kim <dawnbreather@gmail.com>
2024-02-13 16:21:52 -0500

    Fixed systemd and added stop_and_remove container by id endpoint

--- httphandlers/routes.go
+++ httphandlers/routes.go
@@ -2,13 +2,12 @@ package httphandlers
 
 import (
 	"github.com/gorilla/mux"
-	"net/http"
 )
 
 // Define HTTP routes in their own function for better readability
 func defineRoutes(r *mux.Router) {
 	//http.Handle("/", http.FileServer(http.Dir("./front/")))
-	r.PathPrefix("/webterminal").Handler(http.StripPrefix("/webterminal", http.FileServer(http.Dir("./front/"))))
+	//r.PathPrefix("/webterminal").Handler(http.StripPrefix("/webterminal", http.FileServer(http.Dir("./front/"))))
 	r.HandleFunc("/ws", webSocketHandler)
 	r.HandleFunc("/api/admin/container/{id}/extend_ttl/{seconds}", apiAdminContainerExtendTtlHandler).Methods("POST")
 	r.HandleFunc("/api/admin/container/{id}/stop_and_remove", apiAdminContainerStopAndRemoveHandler).Methods("PUT")
